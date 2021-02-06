22449f5003026ed9140b9701927bbe4236d0b6a4
Ivan Luu <ivluu@ucsd.edu>
2021-02-05 18:05:29 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,6 +6,7 @@ var volNum = document.getElementById('volume-number');
 var slider = document.getElementById('volume-slider');
 
 volNum.addEventListener('input', updateVol);
+slider.addEventListener('input', updateSlide);
 
 // Volume and volume image setter
 function updateVol(e) {
@@ -15,18 +16,49 @@ function updateVol(e) {
 
     if (horn.volume > .66) {
         volImg.setAttribute('src','./assets/media/icons/volume-level-3.svg');
+        slider.setAttribute('value', e.target.value);
         console.log('high');
     }
     if (horn.volume > .33 && horn.volume < .67) {
         volImg.setAttribute('src','./assets/media/icons/volume-level-2.svg');
+        slider.setAttribute('value', e.target.value);
         console.log('med');
     }
     if (horn.volume > 0 && horn.volume < .34) {
         volImg.setAttribute('src','./assets/media/icons/volume-level-1.svg');
+        slider.setAttribute('value', e.target.value);
         console.log('low');
     }
     if (horn.volume == 0) {
         volImg.setAttribute('src','./assets/media/icons/volume-level-0.svg');
+        slider.setAttribute('value', e.target.value);
+        console.log('mute');
+    }
+}
+
+function updateSlide(e) {
+    horn.volume = e.target.value / 100;
+    console.log(horn.volume);
+    volNum.setAttribute('change', horn.volume);
+
+    if (horn.volume > .66) {
+        volImg.setAttribute('src','./assets/media/icons/volume-level-3.svg');
+        volNum.setAttribute('value', e.target.value);
+        console.log('high');
+    }
+    if (horn.volume > .33 && horn.volume < .67) {
+        volImg.setAttribute('src','./assets/media/icons/volume-level-2.svg');
+        volNum.setAttribute('value', e.target.value);
+        console.log('med');
+    }
+    if (horn.volume > 0 && horn.volume < .34) {
+        volImg.setAttribute('src','./assets/media/icons/volume-level-1.svg');
+        volNum.setAttribute('value', e.target.value);
+        console.log('low');
+    }
+    if (horn.volume == 0) {
+        volImg.setAttribute('src','./assets/media/icons/volume-level-0.svg');
+        volNum.setAttribute('value', e.target.value);
         console.log('mute');
     }
 }
